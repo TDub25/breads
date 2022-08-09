a8547d88d939e8e4872fd1b569f664e547425de2
Travis Storie <storie25@outlook.com>
2022-08-08 19:07:38 -0700

    Added first part of be-4 activity

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -16,10 +16,10 @@ breads.get('/', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread: Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex]
     })
   } else {
-    res.send('404')
+    res.render('404')
   }
 })
 
--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -8,6 +8,7 @@ function Default(html) {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" integrity="sha512-EZLkOqwILORob+p0BXZc+Vm3RgJBOe1Iq/0fiI7r/wJgzOFZMlsqTa29UEl6v6U6gsV4uIpsNZoV32YZqrCRCQ==" crossOrigin="anonymous" />
       <link rel="stylesheet" href="/main.css" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     </head>
     <body>
       <div className="wrapper">
