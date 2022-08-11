dbe895f73cce0c66d3efee5578de954aef91adf3
Travis Storie <storie25@outlook.com>
2022-08-10 18:42:56 -0700

    Multiple changes

--- views/show.jsx
+++ views/show.jsx
@@ -2,9 +2,8 @@ const React = require('react')
 const Default = require('./layouts/Default')
 
 function Show ({bread}) {
-    // Confirm we are getting our bread data in the terminal.
-    // console.log(bread.name)
-      return (
+    //console.log(bread.name)
+    return(
         <Default>
             <h3>{bread.name}</h3>
             <p>
@@ -16,10 +15,10 @@ function Show ({bread}) {
                 }
                 have gluten.
             </p>
-  <         img src={bread.image} alt={bread.name} />
-            <li><a href="/breads">Go home</a></li>
+            <img src={bread.image} alt={bread.name} />
+            <li><a href="/breads">Go Home</a></li>
         </Default>
-      )
-  }  
+    )
+}
 
 module.exports = Show
