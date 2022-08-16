044df610528404503962652018e233a56eb422a5
Travis Storie <storie25@outlook.com>
2022-08-15 19:00:07 -0700

    Added models and schemas exercise activity

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // DEPENDENCIES
 const express = require('express')
-const methodOverride = require('method-override');
+const { use } = require('./controllers/breads_controller.js')
 const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
-// configuration
+// CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
@@ -14,39 +17,22 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
 
-// routes
-app.get('/', (req,res) => {
- res.send(`
- Welcome to an awsome App about Breads!
- <br>
- <br>
- <a href="breads">Go to Breads page</a>
- `)
-});
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
 
-// MONGOOSE
-mongoose.connect('mongodb://localhost:27017/breads', {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-);
-
-// listen
-app.listen(PORT, () => {
-  console.log("nomming at port", PORT);
-});
-
-// BREADS - Linked up to breads_controller.js
+// Breads
 const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController);
-
-// bakers
-const bakersController = require('./controllers/bakers_controller.js')
-app.use('/bakers', bakersController);
-
-// 404 Page
+app.use('/breads', breadsController)
+  
+//404 Page
 app.get('*', (req, res) => {
   res.send('404')
-});
+})
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
